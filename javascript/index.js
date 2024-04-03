e2c4b524458518f3e0e1d9939316ef16fe843549
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-04-03 01:52:40 +0100

    cleaned up a bit more because why not

--- javascript/index.js
+++ javascript/index.js
@@ -1,13 +1,14 @@
-// Get the navigation bar element
+// Get the elements
 const navBar = document.querySelector('nav');
 const heading = document.querySelector('h1');
+const header = document.querySelector('header');
 
 // Get the offset position of the navigation bar
-const headingOffset = heading.offsetTop;
+// The 200 is just what felt right
+const headerOffset = heading.offsetTop/2 + 200;
 
-// Function to add the 'sticky' class to the navigation bar when it reaches the top of the page
 function stickyNavBar() {
-  if (window.pageYOffset >=  headingOffset) {
+  if (window.pageYOffset >=  headerOffset) {
     navBar.classList.add('sticky');
     heading.classList.add('sticky');
   } else {
