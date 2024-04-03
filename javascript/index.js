cadf35f8964ab29c3e478648360b2c42ff612729
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-04-03 01:36:15 +0100

    simplified code

--- javascript/index.js
+++ javascript/index.js
@@ -3,30 +3,21 @@ const navBar = document.querySelector('nav');
 const heading = document.querySelector('h1');
 
 // Get the offset position of the navigation bar
-const navBarOffset = navBar.offsetTop;
 const headingOffset = heading.offsetTop;
 
 // Function to add the 'sticky' class to the navigation bar when it reaches the top of the page
 function stickyNavBar() {
-  if (window.pageYOffset >= navBarOffset) {
+  if (window.pageYOffset >=  headingOffset) {
     navBar.classList.add('sticky');
-    navBar.classList.add('sticky');
+    heading.classList.add('sticky');
   } else {
     navBar.classList.remove('sticky');
+    heading.classList.remove('sticky');
   }
 }
 
 // Event listener for scrolling to call the stickyNavBar function
 window.onscroll = function() {
   stickyNavBar();
-  stickyh1();
 };
-
-function stickyh1() {
-  if (window.pageYOffset >= headingOffset) {
-    heading.classList.add('sticky');
-  } else {
-    heading.classList.remove('sticky');
-  }
-}
 
