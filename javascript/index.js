22d772d9409658b16dafc67edf93bbe1cace532a
CaptainJack2491 <jayrupnakawala@gmail.com>
2024-04-06 17:40:49 +0100

    daily commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,17 @@
 // Get the elements
-const navBar = document.querySelector('nav');
+const navBar = document.querySelector('.navbar');
 const heading = document.querySelector('h1');
 const header = document.querySelector('header');
 
 // Get the offset position of the navigation bar
 // The 200 is just what felt right
-const headerOffset = heading.offsetTop/2 + 200;
+const navBarOffset = heading.offsetTop;
 
 function stickyNavBar() {
-  if (window.pageYOffset >=  headerOffset) {
+  if (window.pageYOffset >= navBarOffset) {
     navBar.classList.add('sticky');
-    heading.classList.add('sticky');
   } else {
     navBar.classList.remove('sticky');
-    heading.classList.remove('sticky');
   }
 }
 
